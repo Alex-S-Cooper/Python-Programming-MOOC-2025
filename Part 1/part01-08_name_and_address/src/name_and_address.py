3a6306afe2d71bac0a3e99b7311eddc9110c5ebe
Alex-S-Cooper <alex.s.cooper@outlook.com>
2026-03-17 21:35:45 +0000

    exercises 1-10 completed

---
+++ Part 1/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,7 @@
+first_name = input("Given name: ")
+family_name = input("Family name: ")
+street_address = input("Street address: ")
+city_and_postcode = input("City and postal code: ")
+print(first_name + " " + family_name)
+print(street_address)
+print(city_and_postcode)
