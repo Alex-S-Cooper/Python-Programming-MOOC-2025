054412666a21396beb26cad23b966a39a00f77f3
Alex-S-Cooper <alex.s.cooper@outlook.com>
2026-03-18 17:46:22 +0000

    exercises 11-15 completed

---
+++ Part 1/part01-13_times_five/src/times_five.py
@@ -0,0 +1,2 @@
+user_input = int(input("Please type in a number: "))
+print(f"{user_input} times 5 is {user_input * 5}")
---
+++ Part 1/part01-14_name_and_age/src/name_and_age.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ")
+birth_year = int(input("Which year were you born? "))
+print(f"Hi {name}, you will be {2021 - birth_year} years old at the end of the year 2021")
