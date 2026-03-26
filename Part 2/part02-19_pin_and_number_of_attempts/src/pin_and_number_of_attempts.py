3543fa7aab6102ecbe27eae48fa082984bab89bd
Alex-S-Cooper <alex.s.cooper@outlook.com>
2026-03-26 22:13:33 +0000

    part 2, exercises 19 completed

---
+++ Part 2/part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -0,0 +1,16 @@
+correct_pin = 4321
+attempts = 1
+
+while True:
+    pin = int(input("PIN: "))
+    
+    if pin == correct_pin:
+        break
+    else:
+        print("Wrong")
+        attempts += 1
+
+if attempts == 1:
+    print("Correct! It only took you one single attempt!")
+else:
+    print(f"Correct! It took you {attempts} attempts")
